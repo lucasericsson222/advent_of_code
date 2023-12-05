a62867c929456882cff7b75202d01edb768ae229
Lucas Ericsson <lucasericsson222@gmail.com>
2023-12-05 09:42:49 -0800

    part1 in cpp working for day 5!

--- 2023/day_05/part1.cpp
+++ 2023/day_05/part1.cpp
@@ -6,10 +6,38 @@
 #include <unordered_map>
 #include <vector>
 
-std::vector<int> parse_seed_numbers(std::string line); 
-std::unordered_map<int, int> parse_map(std::string start_line, std::vector<std::string>& lines); 
-int get_padded(int in, std::unordered_map<int, int>& map); 
+std::vector<u_int32_t> parse_seed_numbers(std::string line); 
 
+class range {
+public:
+    u_int32_t destination_start, source_start, range_length;
+    range(u_int32_t destination_start, u_int32_t source_start, u_int32_t range_length)
+    : destination_start(destination_start)
+    , source_start(source_start)
+    , range_length(range_length)
+    {}
+};
+class range_map {
+private:
+    std::vector<range> ranges;
+public:
+    void insert(range input_range) {
+        ranges.push_back(input_range);
+    }
+
+    u_int32_t find(u_int32_t in) {
+        for (auto& range : ranges) {
+            
+            int64_t dif = in - range.source_start;
+            if (dif < range.range_length && dif >= 0) {
+                return dif + range.destination_start;
+            }
+        }
+        return in;
+    }
+};
+
+range_map parse_map(std::string start_line, std::vector<std::string>& lines); 
 int main() {
     std::ifstream file("input.txt"); 
     
@@ -26,8 +54,6 @@ int main() {
     }
     
     auto seeds = parse_seed_numbers(lines[0]);
-    std::cout << "First seed: " << seeds[0] << std::endl;
-    std::cout << "Parse seeds is okay" << std::endl;    
 
     auto seeds_to_soil = parse_map("seed-to-soil map:", lines);
     auto soil_to_fertilizer = parse_map("soil-to-fertilizer map:", lines);
@@ -36,69 +62,58 @@ int main() {
     auto light_to_temperature = parse_map("light-to-temperature map:", lines);
     auto temperature_to_humidity = parse_map("temperature-to-humidity map:", lines);
     auto humidity_to_location = parse_map("humidity-to-location map:", lines);
-    std::cout << "Parse map is okay" << std::endl;
 
-    std::vector<int> locations;
+    std::vector<u_int32_t> locations;
     for (auto seed : seeds) {
-        auto soil = get_padded(seed, seeds_to_soil);
-        std::cout << "soil:" << soil << std::endl;
-        auto fertilizer = get_padded(soil, soil_to_fertilizer);
-        auto water = get_padded(fertilizer, fertilizer_to_water);
-        auto light = get_padded(water, water_to_light);
-        auto temp = get_padded(light, light_to_temperature);
-        auto humidity = get_padded(temp, temperature_to_humidity);
-        auto location = get_padded(humidity, humidity_to_location);
+        auto soil = seeds_to_soil.find(seed);
+        auto fertilizer = soil_to_fertilizer.find(soil);
+        auto water = fertilizer_to_water.find(fertilizer);
+        auto light = water_to_light.find(water);
+        auto temp = light_to_temperature.find(light);
+        auto humidity = temperature_to_humidity.find(temp);
+        auto location = humidity_to_location.find(humidity);
         locations.push_back(location);
     }
-    std::cout << "Get locations okay" << std::endl;
-    int min = locations[0];
-    std::cout << "there is a location" << std::endl;
+    u_int32_t min = locations[0];
 
     for (auto location : locations) {
         if (location < min) {
             min = location;
         }
     }
-    std::cout << "there is a min" << std::endl;
 
     std::cout << min << std::endl;
 }
 
-int get_padded(int in, std::unordered_map<int, int>& map) {
-    auto search = map.find(in);
-    if (search != map.end()) {
-        return search->second;
-    }
-    return in;
-}
-
-std::vector<int> parse_seed_numbers(std::string line) {
-    std::vector<int> seeds;
+std::vector<u_int32_t> parse_seed_numbers(std::string line) {
+    std::vector<u_int32_t> seeds;
     
     line.erase(0, 7); // erase "seeds"
     std::stringstream ss(line);
 
-    int number;
+    u_int32_t number;
     while (!ss.eof()) {
         ss >> number;
-        std::cout << "Seed number: " << number << std::endl;
         seeds.push_back(number);
     }
 
     return seeds;
 }
 
-std::unordered_map<int, int> parse_map(std::string start_line, std::vector<std::string>& lines) {
-    std::unordered_map<int, int> map;
+range_map parse_map(std::string start_line, std::vector<std::string>& lines) {
+    range_map map;
     bool is_part_of_map = false;
     for (auto& line : lines) {
         if (line.find(start_line) != std::string::npos) {
             is_part_of_map = true;
             continue;
         }
+        if (!is_part_of_map) {
+            continue;
+        }
         std::stringstream ss(line);
 
-        int destination_start, source_start, range_length;
+        u_int32_t destination_start, source_start, range_length;
 
         if (!(ss >> destination_start)) {
             break;
@@ -106,9 +121,8 @@ std::unordered_map<int, int> parse_map(std::string start_line, std::vector<std::
         ss >> source_start;
         ss >> range_length;
 
-        for (int i = 0; i < range_length; i++) {
-            map.insert({source_start + i, destination_start + i}); 
-        }
+        range new_range = {destination_start, source_start, range_length};
+        map.insert(new_range); 
     }
     return map;
 }
